1e495d461478489a3c771e83978df1370152d463
Joy Christine <joychristin2@gmail.com>
2022-04-04 23:39:11 +0300

    initial commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+}) 
+export class ServerComponent {
+
+}
+// *@Component*  
+// ({
+// selector: 'greet',
+// viewProviders: [
+// Greeter
+// ],
+// template: `<needs-greeter></needs-greeter>`
+// })
